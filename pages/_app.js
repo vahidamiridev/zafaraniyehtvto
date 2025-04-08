4720287be500e32e71f8ec8c8c3b13058208ff32
vahidamiridev <vahidamiri.dev@gmail.com>
2025-04-08 20:52:01 +0330

    [FEAT-main]:added template to next and fixed third library

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import "@/styles/globals.css";
+import LoadExternalScripts from "@/components/LoadExternalScripts";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+      </Head>
+      <LoadExternalScripts />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
